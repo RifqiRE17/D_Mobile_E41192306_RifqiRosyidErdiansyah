7209786a445b7772098b46297911e4ec8584cf2b
RifqiRE17 <72175584+RifqiRE17@users.noreply.github.com>
2021-06-24 08:55:47 +0700

    JsonMovieDB

---
+++ JsonMovieDB/app/src/main/java/com/themoviedb/myapplication/ApiEndpoint.java
@@ -0,0 +1,13 @@
+package com.themoviedb.myapplication;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=853623eb4b73cfd354e9661056d91332";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
